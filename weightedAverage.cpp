493fd3498ba8681513e569c8887d8e3f62b5eba5
apatelDF <40298114+apatelDF@users.noreply.github.com>
2018-06-22 12:09:50 -0500

    typo

--- weightedAverage.cpp
+++ weightedAverage.cpp
@@ -40,7 +40,7 @@ double weightedAverage(double *foo)
 
   for(int i = 0; i < size; i++){
     double maped = map(foo[i], min, max, 0, 100);
-    cout<<maped<<"\n";
+    cout<<"foo["<<i<<"]"<<maped<<"\n";
     sum += foo[i] * maped;
     count += maped;
   }
